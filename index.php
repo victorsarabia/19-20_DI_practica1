3d5f5e952341af65fc219e87be48d025ab41f7d5
Víctor Sarabia <victorsarabia@elcampico.org>
2019-09-30 16:30:21 +0200

    Terminada carga de datos en listado.

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Listado práctica DI 1.1</title>
+	<link rel="stylesheet" type="text/css" href="css/list.css">
+</head>
+<body>
+	<h1>Práctica DI 1.1</h1>
+	<?php require "list.php";?>
+</body>
+</html>
--- list.php
+++ list.php
@@ -1,1 +1,42 @@
 <?php
+require_once "conf.php";
+
+try {
+  $con = new PDO('mysql:host=localhost;dbname='.DB_NAME, DB_USER, DB_PASS);
+  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $stmt = $con->prepare('SELECT * FROM city');
+  $stmt->execute();
+?>
+<table>
+	<thead>
+	<tr>
+		<th>ID</th>
+		<th>Nombre</th>
+		<th>Cód. país</th>
+		<th>Distrito</th>
+		<th>Población</th>
+	</tr>
+	</thead>
+	<tbody>
+	<?php
+	while( $datos = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+    	echo '<tr>';
+    	echo '<td>'. $datos['ID'] .'</td>';
+    	echo '<td>'. $datos['Name'] .'</td>';
+    	echo '<td>'. $datos['CountryCode'] .'</td>';
+    	echo '<td>'. $datos['District'] .'</td>';
+    	echo '<td>'. $datos['Population'] .'</td>';
+    	echo '</tr>';
+	}
+	?>
+	</tbody>
+</table>
+
+<?php  
+} catch(PDOException $e) {
+  echo 'Error: ' . $e->getMessage();
+}
+
+?>
+
